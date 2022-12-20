64348026f109dd3960987dd5f64666a570e8878e
BrokenBoy204 <111448245+Brokenboy204@users.noreply.github.com>
2022-12-20 19:59:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://<anshseth>:<Anshuu12>@cluster0.izecre3.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://brokenboy:brokenboy204@cluster0.mwysc4u.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Asmovies204")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
