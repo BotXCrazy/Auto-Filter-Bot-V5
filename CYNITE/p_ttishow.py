1661f2da2441e5c4500ee09643c40f4105492bb5
BrokenBoy204 <111448245+Brokenboy204@users.noreply.github.com>
2023-01-17 13:08:11 +0530

    Update p_ttishow.py

--- CYNITE/p_ttishow.py
+++ CYNITE/p_ttishow.py
@@ -56,7 +56,7 @@ async def save_group(bot, message):
         if settings["welcome"]:
             for u in message.new_chat_members:
                 buttons = [[
-                InlineKeyboardButton("ɢʀᴏᴜᴘ📌", url="https://t.me/cynitesupport")
+                InlineKeyboardButton("ɢʀᴏᴜᴘ📌", url="https://t.me/TG_Updates_204")
             ]]
                 if (temp.MELCOW).get('welcome') is not None:
                     try:
