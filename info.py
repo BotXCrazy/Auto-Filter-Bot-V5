b2e8f3f125d0a7b4e4da95cb577289a5d61338e6
BotXCrazy <129079578+BotXCrazy@users.noreply.github.com>
2023-06-27 10:18:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP',-1001780651238))
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', -1001780651238))
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 'https://t.me/+QJ0zdNuBAh5lNWI9')
-HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/how_to_open_link204/7')
+HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/how_to_open_link204/13')
 
 # Log Channels
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001567945519))
